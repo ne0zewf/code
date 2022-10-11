3bc1eea91884a21c154880d8327d250e3add2a5f
ne0zewf <115514608+ne0zewf@users.noreply.github.com>
2022-10-11 08:26:40 +0700

    Add files via upload

---
+++ Anti spam discord bot.py
@@ -0,0 +1,31 @@
+from discord import Intents
+from discord.ext import commands
+import asyncio
+
+client = commands.Bot(command_prefix="?",intents=Intents. all())
+@client.event
+async def on_ready():
+    print("Online")
+    while True:
+        print("cleared")
+        await asyncio.sleep(10)
+        with open("spam_detect.txt", "r+") as file:
+            file.truncate(0)
+
+@client.event
+async def on_message(message):
+    counter = 0
+    with open("spam_detect.txt", "r+") as file:
+        for lines in file:
+            if lines.strip("\n") == str(message.author.id):
+                counter+=1
+    
+        file.writelines(f"{str(message.author.id)}\n")
+        if counter > 2:
+            await message.guild.ban(message.author, reason = "spam")
+            await asyncio.sleep(10)
+            await message.guild.unban(message.author)
+            print("Banned!")
+    
+client.run("Token is here")
+
